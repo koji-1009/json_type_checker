9ec40c7ecdf2da1f713d9448a0a80d90531f70fe
Koji Wakamiya <koji.wakamiya@gmail.com>
2023-05-03 17:07:05 +0900

    cleanup

--- module/checker/chcker.go
+++ module/checker/chcker.go
@@ -2,16 +2,11 @@ package checker
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"os"
 )
 
-func main() {
-	fmt.Println("Hello, World!")
-}
-
-/// read json file from file path
+// read json file from file path
 func ReadJsonFile(filePath string) (map[string]interface{}, error) {
 	jsonFile, err := os.Open(filePath)
 	if err != nil {
